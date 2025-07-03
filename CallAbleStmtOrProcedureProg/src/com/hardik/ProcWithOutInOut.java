c05177f9a6e2eab3fc6ca1e6d7767f6550af7c72
Hardik585 <Hardikkaushik83126@gmail.com>
2025-07-03 19:11:22 +0530

    learn about the purpose of the callable statement and Procedure in DBMS

---
+++ CallAbleStmtOrProcedureProg/src/com/hardik/ProcWithOutInOut.java
@@ -0,0 +1,28 @@
+package com.hardik;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ProcWithOutInOut {
+
+	private static final String DB_URL = "jdbc:mysql://localhost:3306/advjava";
+	private static final String DB_USER = "hardik";
+	private static final String DB_PASSWORD = "Hardik@123";
+	private static final String PROC_CALL = "call getBookData()";
+
+	public static void main(String[] args) throws SQLException {
+		Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+		Statement stmt = con.createStatement();
+		ResultSet rs = stmt.executeQuery(PROC_CALL);
+		while (rs.next()) {
+			System.out.print("BOOK id is : " + rs.getInt(1));
+			System.out.print("  BOOK name is : " + rs.getString(2));
+			System.out.print("  BOOK price is : " + rs.getFloat(3));
+			System.out.println();
+		}
+		con.close();
+	}
+}
